aa4e0fceba902c1c778d128df06608a8e72356c1
stubbornmarlin3 <arcarter129@gmail.com>
2024-10-04 01:21:18 -0500

    changed optimizer to SGD with momentum

--- train_flow.py
+++ train_flow.py
@@ -6,7 +6,7 @@ from dataset import Dataset
 from i3d import I3D
 import torch
 
-dev = torch.device("cpu")
+dev = torch.device("cuda")
 
 def main():
     train = Dataset("./MS-ASL/MSASL_train.json", "./MS-ASL/MSASL_classes.json", "./Train")
@@ -14,7 +14,7 @@ def main():
 
     model = I3D().to(dev)
 
-    optim = torch.optim.Adam(model.parameters(), lr=1e-3)
+    optim = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
 
     num_epochs = 100
 
@@ -26,7 +26,7 @@ def main():
         avg_loss = 0.0
 
         # Train
-        for i in range(1):
+        for i in range(train.num_samples):
             print(f"\rTraining: {((i+1)/train.num_samples)*100:.3f}%  |  Video Index: {i}", end="", flush=True)
 
             # Get input and class
@@ -67,39 +67,39 @@ def main():
 
         # Evaluations on validation set
 
-        # all_predict = []
-        # all_truth = []
+        all_predict = []
+        all_truth = []
 
-        # with torch.no_grad():
-        #     for i in range(valid.num_samples):
-        #         print(f"\rValidating: {((i+1)/valid.num_samples)*100:.3f}%  |  Video Index: {i}", end="", flush=True)
+        with torch.no_grad():
+            for i in range(valid.num_samples):
+                print(f"\rValidating: {((i+1)/valid.num_samples)*100:.3f}%  |  Video Index: {i}", end="", flush=True)
 
-        #         if not valid.downloadVideo(i):
-        #             continue
+                if not valid.downloadVideo(i):
+                    continue
 
-        #         try:
-        #             input = valid.extractFrames(i)[:,:64,:,:].to(dev)
-        #         except RuntimeError:    # For if frames cannot be extracted ie corrupt video
-        #             continue
-        #         label = valid.getLabel(i).to(dev)
+                try:
+                    input = valid.extractFrames(i)[:,:64,:,:].to(dev)
+                except RuntimeError:    # For if frames cannot be extracted ie corrupt video
+                    continue
+                label = valid.getLabel(i).to(dev)
 
-        #         # Extend frames if less than 64
-        #         if input.shape[1] < 64:
-        #             input = torch.cat((input, input[:,-1:,:,:].repeat(1,(64 - input.size(1)),1,1)), 1)
+                # Extend frames if less than 64
+                if input.shape[1] < 64:
+                    input = torch.cat((input, input[:,-1:,:,:].repeat(1,(64 - input.size(1)),1,1)), 1)
 
-        #         predict = model(input)
-        #         all_predict.append(torch.argmax(predict))
-        #         all_truth.append(torch.argmax(label))
+                predict = model(input)
+                all_predict.append(torch.argmax(predict))
+                all_truth.append(torch.argmax(label))
 
-        # all_predict = torch.stack(all_predict)
-        # all_truth = torch.stack(all_truth)
+        all_predict = torch.stack(all_predict)
+        all_truth = torch.stack(all_truth)
 
-        # valid_acc = torch.sum(all_predict ==  all_truth) / valid.num_samples * 100
-        # print(f"\nValidation accuracy: {valid_acc:.3f}%")
+        valid_acc = torch.sum(all_predict ==  all_truth) / valid.num_samples * 100
+        print(f"\nValidation accuracy: {valid_acc:.3f}%")
 
-        # if valid_acc > best_acc or epoch == 0:
-        #     best_acc = valid_acc
-        #     torch.save(model.state_dict(), "./I3D_RGB_model.dat")
+        if valid_acc > best_acc or epoch == 0:
+            best_acc = valid_acc
+            torch.save(model.state_dict(), "./I3D_RGB_model.dat")
 
 
 if __name__ == "__main__":
