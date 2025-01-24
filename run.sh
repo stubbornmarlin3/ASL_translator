8ccf4a104dda8946a597b72202bb5bb0086d8d9b
stubbornmarlin3 <arcarter129@gmail.com>
2025-01-24 15:22:33 -0500

    print to terminal

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ index=0
 
 while true; do
     rm cookies.txt
-    index=$($HOME/test/bin/python dataset.py $index | tail -n 1)
+    index=$($HOME/test/bin/python dataset.py $index | tee >(cat) / tail -n 1)
     
     if [ $index -eq 0 ]; then
         break
