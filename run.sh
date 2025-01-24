ef47daa13ac3a4aad2c1a3f9d73d4c9c65ca5cc0
stubbornmarlin3 <arcarter129@gmail.com>
2025-01-24 14:48:25 -0500

    fixed script

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+exit_code=0
+
 while true; do
     rm cookies.txt
-    $HOME/test/bin/python dataset.py $?
+    $HOME/test/bin/python dataset.py $exit_code
     exit_code=$?
     if [ $exit_code -eq 0 ]; then
         break
