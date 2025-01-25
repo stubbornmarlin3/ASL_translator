757f15f6da85ae16050f78b758c1ede9102a20ce
stubbornmarlin3 <arcarter129@gmail.com>
2025-01-25 03:17:05 -0500

    fixed run script

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ index=0
 
 while true; do
     rm cookies.txt
-    index=$($HOME/test/bin/python dataset.py $index | tee >(cat) | tail -n 1)
+    index=$($HOME/test/bin/python dataset.py $index | tee >/dev/tty | tail -n 1)
     exit_code=$?
     if [ $exit_code -eq 0 ]; then
         break
