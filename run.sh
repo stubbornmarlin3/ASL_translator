d758f2d4370d55bfa1cbedfc480e3a8b54cad85a
stubbornmarlin3 <arcarter129@gmail.com>
2025-01-26 00:30:15 -0500

    idk

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@ index=0
 
 while true; do
     rm cookies.txt
-    index=$($HOME/test/bin/python dataset.py $index | tee >/dev/tty | tail -n 1)
-    if [ $index -eq 0 ]; then
+    index=$($HOME/test/bin/python dataset.py $index | tee /dev/tty | tail -n 1)
+
+    if [[ "$index" -eq 0 ]]; then
         break
     else
         sleep 5
