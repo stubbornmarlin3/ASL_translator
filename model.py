c9c26d3cb2011482cd6b10f9a3a9d1541f8f7bd3
stubbornmarlin3 <arcarter129@gmail.com>
2025-01-31 15:10:13 -0500

    testing

--- model.py
+++ model.py
@@ -77,8 +77,7 @@ class ASLModel:
                 torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
 
                 for name, param in self.model.named_parameters():
-                    if param.grad is None:
-                        print(f"Warning: {name} has no gradients!")
+                    print(f"{name} - {param.grad}")
 
                 for name, param in self.model.named_parameters():
                     writer.add_histogram(f'gradients/{name}', param.grad, epoch)
