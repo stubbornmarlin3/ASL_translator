2e3512aea1f90ece84f69ebaf9edb2cbbee55cd0
stubbornmarlin3 <arcarter129@gmail.com>
2025-04-25 12:57:30 -0400

    test

--- demo2.py
+++ demo2.py
@@ -1,23 +1,45 @@
 import cv2
 import numpy as np
 import torch
+from collections import deque
 from asl import ASL
-from torchcodec.decoders import VideoDecoder
+import time
+import threading
 
 classes = ["hello", "nice", "teacher", "eat", "no", "happy", "like", "orange", "want", "deaf", "school"]
+currentPred = ""
+threadLock = threading.Lock()
+threadRunning = False
 
-if __name__ == "__main__":
+def modelEval(frameBuffer):
+
+    global currentPred, threadRunning
+    with threadLock:
+
+        X = np.stack(frameBuffer, axis=0)
+        X = torch.tensor(X, dtype=torch.float32).unsqueeze(0)
+        X = X.permute(0,4,1,2,3) / 255.0
+
+        with torch.no_grad():
+            pred = model(X)
+            pred = torch.argmax(pred, dim=1).item()
+            currentPred = classes[pred]
+        threadRunning = False
+
+def cameraPredict(model):
+    global threadRunning, currentPred
+    lastPredTime = time.time()
     camera = cv2.VideoCapture(0)
-    outputFlow = cv2.VideoWriter("test.mp4", cv2.VideoWriter_fourcc(*"mp4v"), camera.get(cv2.CAP_PROP_FPS), (224, 224))
+    frameBuffer = deque(maxlen=64)
     prevGray = None
 
-    for i in range(65):
+    while True:
         ret, frame = camera.read()
 
         if not ret:
             break
-
-        height, width, _ = frame.shape
+        
+        height, width, channels = frame.shape
         frame = frame[(height-1080)//2:(height+1080)//2, (width-1080)//2:(width+1080)//2]
 
         output = frame
@@ -54,31 +76,34 @@ if __name__ == "__main__":
             # Convert the colors from HSV to RGB
             frame = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
-            outputFlow.write(frame)
+            frameBuffer.append(frame)
 
         prevGray = gray
 
+        if not threadRunning and len(frameBuffer) == 64 and (time.time()-lastPredTime >= 0.5):
+
+            threadRunning = True
+            lastPredTime = time.time()
+
+            thread = threading.Thread(
+                target = modelEval,
+                args=(list(frameBuffer),),
+            )
+            thread.start()
+
+        cv2.putText(output, f"{currentPred}", (450, 1000), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 50, 0), 2)
         cv2.imshow("Camera", output)
         cv2.imshow("Optical Flow", frame)
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     camera.release()
-    outputFlow.release()
     cv2.destroyAllWindows()
 
-    decoder = VideoDecoder("test.mp4")
-    frames = decoder[:]
-    assert len(frames) == 64
 
-    framesDec = torch.stack([frame for frame in frames], dim=1) / 255.0
 
+if __name__ == "__main__":
     model = ASL(10)
-    model.load_state_dict(torch.load("./Models/2025-04-12_11-06-26_10_71-429_flow.ASL", map_location="cpu"))
+    model.load_state_dict(torch.load("./runs/Apr11_14-13-34_serverpc/2025-04-12_11-06-26_10_71-429_flow.ASL", map_location="cpu"))
     model.eval()
-
-    with torch.no_grad():
-        pred = model(torch.stack((framesDec, framesDec, framesDec, framesDec)))
-        pred = torch.argmax(pred, dim=1)
-        print(pred)
+    cameraPredict(model)
